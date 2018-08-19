d7a4ecb460080602147b4465840f738ce721b8c4
Lucas Santos <lhs.santoss@gmail.com>
2018-08-19 18:05:46 -0300

    ✨ Add index file

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const logger = require('knoblr')
-const getRandomInt = require('./src/utils/getRandomInt')
-const messages = require('./src/config/messageStrings')
-const net = require('net')
+const argList = process.argv[2] ? process.argv[2].split(',') : []
 
-const server = net.createServer()
-server.listen(process.argv[2], '127.0.0.1', () =>
-  logger.info(`Server listening on ${server.address().address}:${server.address().port}`)
-)
-
-server.on('connection', (socket) => {
-  logger.info(`The client ${socket.remoteAddress}:${socket.remotePort} has connected to this server`)
-
-  socket.on('data', (data) => {
-    logger.info(`Received data from ${socket.remoteAddress}:${socket.remotePort}: "${data}"`)
-    socket.write(messages.ACK)
-  })
-
-  socket.on('close', (data) => {
-    logger.warn(`The client ${socket.remoteAddress}:${socket.remotePort} has disconnected from this server`)
-  })
+const nodeList = argList.map((nodeAddress) => {
+  const [address, port] = nodeAddress.split(':')
+  return {
+    nodeAddress: address,
+    nodePort: parseInt(port, 10)
+  }
 })
 
-const stdin = process.openStdin()
-stdin.addListener('data', (data) => {
-  if (data.length <= 1) return logger.info('Empty data not sent')
-  const message = require('./src/utils/hashFactory').generateHashFrom(data.toString().replace(/\n$/, ''))
-  require('./src/utils/socketClient').sendMessageTo('127.0.0.1', 4567, message)
-})
+require('./src/node').start(nodeList)
