1d4b0e5920106bc7fb534722bd95781de06df30c
Lucas Santos <lhs.santoss@gmail.com>
2018-08-19 18:06:25 -0300

    :sparkles: Add socket server wrapper

--- src/utils/socketServer.js
+++ src/utils/socketServer.js
@@ -1,6 +1,5 @@
 const net = require('net')
 const getRandomInt = require('./getRandomInt')
-const messages = require('../config/messageStrings')
 
 /**
  * Cria e retorna o servidor TCP
@@ -9,12 +8,11 @@ function createServer () {
   return net.createServer()
 }
 
-/**
- * Atrela um evento a um socket com uma função de tratamento
- * @param {{socket: Socket, event: string, handler: function}} options Objeto de configuração
- * @returns {Socket} O socket enviado
- */
-function bindEventToSocket (options) {
-  const { socket, event, handler } = options
-  return socket.on(event, handler)
+function getPort () {
+  return getRandomInt(3000, 65000)
+}
+
+module.exports = {
+  createServer,
+  getPort
 }
