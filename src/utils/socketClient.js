1b0dc3451609a40c7b366879b6911ea5a47bc68e
Lucas Santos <lhs.santoss@gmail.com>
2018-08-19 18:06:15 -0300

    :sparkles: Add socket client wrapper

--- src/utils/socketClient.js
+++ src/utils/socketClient.js
@@ -1,17 +1,91 @@
 const net = require('net')
+const commandMessages = require('../config/messageStrings')
+const { generateHashFrom } = require('./hashFactory')
 
-function sendMessageTo (address, port, message) {
+/**
+ * Converte mensagens em objetos para buffers para podermos enviar como binário
+ * @param {string|Object} message Mensagem a ser convertida
+ */
+function _convert (message) {
+  let innerMessage = null
+
+  switch (typeof innerMessage) {
+    case 'string':
+      innerMessage = message.replace(/\n$/, '')
+      break
+    case 'object':
+      innerMessage = Buffer.from(JSON.stringify(message))
+      break
+    default:
+      innerMessage = Buffer.from(message)
+      break
+  }
+
+  return innerMessage
+}
+
+function handleError (err) {
+  throw err
+}
+
+/**
+ *
+ * @param {string} address Endereço do destinatário
+ * @param {number} port Porta do destinatário
+ * @param {string} command String do comando
+ * @param {*} params Parâmetros do comando
+ */
+function sendCommandTo (address, port, command, params, errorCb = handleError) {
+  const fullCommand = {
+    commandString: command,
+    commandParams: params
+  }
+  sendMessageTo(address, port, fullCommand, errorCb)
+}
+
+/**
+ * Cria os dados que serão enviados com o comando para padronização
+ * @param {string} command String do comando
+ */
+function createCommandPayload (command) {
+  switch (command) {
+    case commandMessages.JOIN:
+      return (address, port) => ({ nodeAddress: address, nodePort: parseInt(port, 10) })
+    case commandMessages.JOIN_ACK:
+      return (previousNode) => ({
+        previousNode: {
+          port: previousNode.port,
+          address: previousNode.address,
+          id: generateHashFrom(`${previousNode.address}:${previousNode.port}`)
+        },
+        nextNode: {
+          port: global.PORT,
+          address: global.ADDRESS,
+          id: global.myId
+        }
+      })
+  }
+}
+
+/**
+ * Envia uma mensagem a um nó
+ * @param {string} address Endereço do destinatário
+ * @param {number} port Porta do destinarátio
+ * @param {string|Buffer} message Mensagem a ser enviada
+ */
+function sendMessageTo (address, port, message, errorCb) {
   const client = new net.Socket()
+  let innerMessage = _convert(message)
 
-  client.on('error', (err) => {
-    throw err
-  })
+  client.on('error', errorCb)
 
   client.connect(port, address)
-  client.write(message)
+  client.write(innerMessage)
   client.end()
 }
 
 module.exports = {
-  sendMessageTo
+  sendMessageTo,
+  sendCommandTo,
+  createCommandPayload
 }
