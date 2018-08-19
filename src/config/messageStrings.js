a26b8b67017104433c3e729a4864b1d616783f30
Lucas Santos <lhs.santoss@gmail.com>
2018-08-18 22:55:07 -0300

    :sparkles: Add string commands

--- src/config/messageStrings.js
+++ src/config/messageStrings.js
@@ -1,8 +1,11 @@
 module.exports = {
-  JOIN: 'JOIN',
-  JOIN_ACK: 'JOIN_ACK',
-  ACK: 'ACK',
+  JOIN: 'JOIN', // Entrada na rede (enviado pelo próprio nó quando quiser participar para um nó conhecido da rede)
+  JOIN_ACK: 'JOIN_ACK', // Resposta de entrada na rede (Respondido pelo nó da rede que será o )
+  NEW_NODE: 'NEW_NODE',
+  NODE_GONE: 'NODE_GONE',
   RETRIEVE: 'RETRIEVE',
+  FOUND: 'FOUND',
+  NOT_FOUND: 'NOT_FOUND',
   STORE: 'STORE',
   LEAVE: 'LEAVE'
 }
