bc4a26dea4e96bcc2552af23d5e7d67c73586c95
Lucas Santos <lhs.santoss@gmail.com>
2018-08-20 20:12:50 -0300

    :sparkles: Implement leave command

--- src/consoleCommands/leave.js
+++ src/consoleCommands/leave.js
@@ -1,6 +1,16 @@
 const logger = require('knoblr')
+const outSocket = require('../utils/socketClient')
+const messageCommand = require('../config/messageStrings')
 
 module.exports = (params) => {
+  // Envia a mensagem de saída ao próximo nó
+  outSocket.sendCommandTo(
+    global.nextNode.ip,
+    global.nextNode.port,
+    messageCommand.LEAVE,
+    outSocket.createCommandPayload(messageCommand.LEAVE)(global.previousNode)
+  )
   logger.info(`You have been disconnected from the DHT`)
   logger.info(`Exiting... Bye!`)
+  process.exit(0)
 }
