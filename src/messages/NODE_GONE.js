e1784a0abda062c4b9ff4a7315f8497d65f909c1
Lucas Santos <lhs.santoss@gmail.com>
2018-08-20 20:12:41 -0300

    :sparkles: Implement NODE_GONE protocol

--- src/messages/NODE_GONE.js
+++ src/messages/NODE_GONE.js
@@ -1,1 +1,14 @@
-module.exports = (params) => {}
+module.exports = (params) => {
+  // Quando só houver dois nós, se um sair então o outro deve zerar seus nós
+  if (params.nextId === global.myId) {
+    global.previousNode = { id: null, ip: null, port: null }
+    global.nextNode = { id: null, ip: null, port: null }
+    return
+  }
+
+  global.nextNode = {
+    id: params.nextId,
+    ip: params.nextIp,
+    port: params.nextPort
+  }
+}
