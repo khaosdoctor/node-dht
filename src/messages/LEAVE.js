69d0c988d776327df7b97f9a264f0b3a7e5ceb96
Lucas Santos <lhs.santoss@gmail.com>
2018-08-20 20:11:58 -0300

    :sparkles: Implement LEAVE protocol

--- src/messages/LEAVE.js
+++ src/messages/LEAVE.js
@@ -1,1 +1,22 @@
-module.exports = (params) => {}
+const outSocket = require('../utils/socketClient')
+const messageCommand = require('../config/messageStrings')
+
+module.exports = (params) => {
+  // Quando só houver dois nós, se um sair então o outro deve zerar seus nós
+  if (params.newPreviousNode.id === global.myId) {
+    global.previousNode = { id: null, ip: null, port: null }
+    global.nextNode = { id: null, ip: null, port: null }
+    return
+  }
+
+  global.previousNode = params.newPreviousNode
+
+  if (global.previousNode.ip) {
+    outSocket.sendCommandTo(
+      global.previousNode.ip,
+      global.previousNode.port,
+      messageCommand.NODE_GONE,
+      outSocket.createCommandPayload(messageCommand.NODE_GONE)()
+    )
+  }
+}
