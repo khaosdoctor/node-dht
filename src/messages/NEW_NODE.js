71456e2ee75ddb70bed3e6a6b11a4fe54a083a50
Lucas Santos <lhs.santoss@gmail.com>
2018-08-20 20:12:12 -0300

    :sparkles: Implement NEW_NODE protocol

--- src/messages/NEW_NODE.js
+++ src/messages/NEW_NODE.js
@@ -1,1 +1,8 @@
-module.exports = (params) => {}
+module.exports = (params) => {
+  global.nextNode = {
+    ip: params.ingressAddress,
+    port: params.ingressPort,
+    id: params.ingressId
+  }
+  console.log('Recebido NEW_NODE, novo nó seguinte é:', global.nextNode)
+}
