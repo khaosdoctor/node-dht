9319cd62ba86c0c92b1bcb855477160a0ac99a34
Lucas Santos <lhs.santoss@gmail.com>
2018-08-21 00:16:38 -0300

    :sparkles: Add stats of received messages

--- src/consoleCommands/info.js
+++ src/consoleCommands/info.js
@@ -1,9 +1,14 @@
 const logger = require('knoblr')
 module.exports = (params) => {
-  logger.info(`Previous Node Information: `)
+  logger.info('Printing information')
+  console.log(`--- Previous Node Information: `)
   console.log(global.previousNode)
-  logger.info(`Current Node Information:`)
+  console.log(`--- Current Node Information:`)
   console.log({ ip: global.ADDRESS, port: global.PORT, id: global.myId })
-  logger.info(`Next Node Information:`)
+  console.log(`--- Next Node Information:`)
   console.log(global.nextNode)
+  console.log(`--- Current File List:`)
+  console.log(global.fileList)
+  console.log(`--- Total received messages:`)
+  console.log(global.stats)
 }
